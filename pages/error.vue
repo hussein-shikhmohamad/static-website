8ca653c038ca75b38098d5d63adc05229830ef2c
MattIPv4 <me@mattcowley.co.uk>
2020-07-16 18:32:04 +0100

    eslint

--- pages/error.vue
+++ pages/error.vue
@@ -19,21 +19,21 @@
         head () {
             return setMeta(meta, this);
         },
-        mounted() {
-            this.$data.aaa = 'hello';
-            this.$data.ccc = null;
-        },
-        methods: {
-            error() {
-                this.$data.bbb = this.$data.ccc.slice(0, 1);
-            },
-        },
-        data() {
+        data () {
             return {
                 aaa: 'aaa',
                 bbb: 'bbb',
                 ccc: 'ccc',
             };
+        },
+        mounted () {
+            this.$data.aaa = 'hello';
+            this.$data.ccc = null;
+        },
+        methods: {
+            error () {
+                this.$data.bbb = this.$data.ccc.slice(0, 1);
+            },
         },
     };
 </script>
