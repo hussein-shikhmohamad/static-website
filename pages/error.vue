3b0ba1ca79bed20c0cbf1c8c70d9d75312ecf713
MattIPv4 <me@mattcowley.co.uk>
2020-07-16 16:38:01 +0100

    Generate purposeful error

---
+++ pages/error.vue
@@ -0,0 +1,39 @@
+<template>
+    <section>
+        <a @click="error">{{ aaa }}</a>
+    </section>
+</template>
+
+<script>
+    import setMeta from '../util/set_meta';
+
+    const meta = {
+        title: '',
+        breadcrumb: '',
+        classes: [''],
+    };
+
+    export default {
+        name: 'Error',
+        meta,
+        head () {
+            return setMeta(meta, this);
+        },
+        mounted() {
+            this.$data.aaa = 'hello';
+            this.$data.ccc = null;
+        },
+        methods: {
+            error() {
+                this.$data.bbb = this.$data.ccc.slice(0, 1);
+            },
+        },
+        data() {
+            return {
+                aaa: 'aaa',
+                bbb: 'bbb',
+                ccc: 'ccc',
+            };
+        },
+    };
+</script>
